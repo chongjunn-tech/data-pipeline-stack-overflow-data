d22497895ee927c835be3a612a211764a6b55d10
Chong Junn Teo <chongjunn92@gmail.com>
2022-12-21 09:19:08 +0800

    edit docstrings for PipelineFactory

--- src/pipeline_factory.py
+++ src/pipeline_factory.py
@@ -6,17 +6,26 @@ from src.pandas_on_spark_pipeline import PandasOnSparkPipeline
 ############
 # Functions#
 ############
-def PipelineFactory(pipeline_choice: str, input_folder: str, output_folder:str):
-    """Factory Method to generate pipeline
-    Available choices for pipeline includes:
-        - "pandas": PandasPipeline
-        - "pyspark" : PysparkPipeline
+def PipelineFactory(pipeline_choice: str, input_folder: str , output_folder:str):
     """
+    Factory Method to generate pipeline as created by Data Engineer
+
+    Args:
+        pipeline_choice (str): Available choices for pipeline includes: "pandas", "pyspark"
+        input_folder (str): path to input folder
+        output_folder (str): path to output folder
+
+    Returns:
+        BasePipeline
+    """    
+
     pipelines_available = {
         "pandas": PandasPipeline,
         "pyspark": PysparkPipeline,
         # "pandas_on_spark": PandasOnSparkPipeline, WIP as does not seems to have method to calculate most frequent element
     }
+    if pipeline_choice not in pipeline_choice.keys():
+        raise KeyError(f"pipeline choice not in {list(pipelines_available.keys())}")
     return pipelines_available.get(pipeline_choice)(
         input_folder = input_folder,
         output_folder = output_folder)
